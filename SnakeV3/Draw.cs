ebec8f792a6e3d42c1ddbc65194973e696660b7f
LukaStuber <pajo024@gmail.com>
2025-05-06 00:34:54 +0200

    sfsdfsdf

---
+++ SnakeV3/Draw.cs
@@ -0,0 +1,196 @@
+﻿namespace Snake
+{
+    partial class Game
+    {
+        void DrawBoard(int[,] board)
+        {
+
+        }
+
+        // Menus
+        void DrawTitle()
+        {
+            for (int i = 0; i < title.Length; i++)
+            {
+                Console.CursorLeft = Console.WindowWidth / 2 - title[i].Length / 2;
+                Console.ForegroundColor = i % 2 == 0 ? ConsoleColor.Red : ConsoleColor.DarkRed;
+                Console.WriteLine(title[i]);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        void DrawItems(Item[] items, int selected)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine();
+                }
+
+                if (selected == i)
+                {
+                    Console.CursorLeft = Console.WindowWidth / 2 - (items[i].Name.Length + 4) / 2;
+                    Console.WriteLine($"[ {items[i].Name} ]");
+                }
+                else
+                {
+                    Console.CursorLeft = Console.WindowWidth / 2 - items[i].Name.Length / 2;
+                    Console.WriteLine(items[i].Name);
+                }
+            }
+        }
+
+        void DrawMainMenu()
+        {
+            Console.Clear();
+            DrawTitle();
+            DrawItems(mainMenuItems, mainMenuSelected);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            input = Console.ReadKey(true).Key;
+
+            if (input == ConsoleKey.DownArrow)
+            {
+                if (mainMenuSelected == mainMenuItems.Length - 1)
+                {
+                    mainMenuSelected = 0;
+                }
+                else
+                {
+                    mainMenuSelected++;
+                }
+            }
+            else if (input == ConsoleKey.UpArrow)
+            {
+                if (mainMenuSelected == 0)
+                {
+                    mainMenuSelected = mainMenuItems.Length - 1;
+                }
+                else
+                {
+                    mainMenuSelected--;
+                }
+            }
+
+            if (input == ConsoleKey.Enter)
+            {
+                switch (mainMenuSelected)
+                {
+                    case 0:
+                        gameState = GameState.Start;
+                        break;
+                    case 1:
+                        gameState = GameState.Settings;
+                        break;
+                    case 2:
+                        gameState = GameState.Quit;
+                        break;
+                }
+                mainMenuSelected = 0;
+            }
+        }
+
+        void DrawSettings()
+        {
+            Console.Clear();
+            DrawTitle();
+            DrawItems(settingsItems, settingsSelected);
+
+            input = Console.ReadKey(true).Key;
+
+            if (input == ConsoleKey.DownArrow)
+            {
+                if (settingsSelected == settingsItems.Length - 1)
+                {
+                    settingsSelected = 0;
+                }
+                else
+                {
+                    settingsSelected++;
+                }
+            }
+            else if (input == ConsoleKey.UpArrow)
+            {
+                if (settingsSelected == 0)
+                {
+                    settingsSelected = settingsItems.Length - 1;
+                }
+                else
+                {
+                    settingsSelected--;
+                }
+            }
+
+            if (input == ConsoleKey.Enter)
+            {
+                switch (settingsSelected)
+                {
+                    case 0:
+                        gameState = GameState.SettingsBoard;
+                        break;
+                    case 1:
+                        gameState = GameState.SettingsSnake;
+                        break;
+                    case 2:
+                        gameState = GameState.SettingsApples;
+                        break;
+                    case 3:
+                        gameState = GameState.MainMenu;
+                        break;
+                }
+                settingsSelected = 0;
+            }
+        }
+
+        void DrawSettingsBoard()
+        {
+            Console.Clear();
+            DrawTitle();
+            DrawItems(settingsBoardItems, settingsBoardSelected);
+
+            input = Console.ReadKey().Key;
+
+            if (input == ConsoleKey.DownArrow)
+            {
+                if (settingsBoardSelected == settingsBoardItems.Length - 1)
+                {
+                    settingsBoardSelected = 0;
+                }
+                else
+                {
+                    settingsBoardSelected++;
+                }
+            }
+            else if (input == ConsoleKey.UpArrow)
+            {
+                if (settingsBoardSelected == 0)
+                {
+                    settingsBoardSelected = settingsBoardItems.Length - 1;
+                }
+                else
+                {
+                    settingsBoardSelected--;
+                }
+            }
+
+            if (input == ConsoleKey.Enter)
+            {
+                switch (settingsBoardSelected)
+                {
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+                        gameState = GameState.Settings;
+                        break;
+
+                }
+                settingsSelected = 0;
+            }
+        }
+    }
+}
--- SnakeV3/Game.cs
+++ SnakeV3/Game.cs
@@ -1,6 +1,8 @@
-﻿namespace Snake
+﻿using System.Net.Http.Headers;
+
+namespace Snake
 {
-    class Game
+    partial class Game
     {
         enum GameState
         {
@@ -19,14 +21,31 @@
 
         struct Snake
         {
-            public int x;
-            public int y;
-            public int length;
+            public int X;
+            public int Y;
+            public int Length;
+        }
+
+        struct Item
+        {
+            public string Name;
+            public bool IsField;
+
+            public Item(string name)
+            {
+                Name = name;
+                IsField = false;
+            }
+
+            public Item(string name, bool isField)
+            {
+                Name = name;
+                IsField = isField;
+            }
         }
 
         GameState gameState;
         int[,] board;
-        readonly string version = "0.1.0";
         readonly string[] title =
         [
             "   ▄████████ ███▄▄▄▄      ▄████████    ▄█   ▄█▄    ▄████████",
@@ -38,208 +57,26 @@
             "   ▄█    ███ ███   ███   ███    ███   ███ ▀███▄   ███    ███",
             " ▄████████▀   ▀█   █▀    ███    █▀    ███   ▀█▀   ██████████"
         ];
-        readonly string[] mainMenuItems = ["Play", "Settings", "Leaderboard", "Quit"];    
+        readonly Item[] mainMenuItems = [new Item("Play"), new Item("Settings"), new Item("Leaderboard"), new Item("Quit")];    
         int mainMenuSelected = 0; // 0 = play, 1 = settings, 2 = quit
-        readonly string[] settingsItems = ["Board", "Snake", "Apples", "Back"];
+        readonly Item[] settingsItems = [new Item("Board"), new Item("Snake"), new Item("Apples"), new Item("Back")];
         int settingsSelected = 0; // 0 = board, 1 = snake, 2 = apples
-        readonly string[] settingsBoardItems = ["Width: [ ]", "Height: [ ]", "Back"];
+        readonly Item[] settingsBoardItems = [new Item("Width: [ ]", true), new Item("Height: [ ]", true), new Item("Back")];
         int settingsBoardSelected = 0; // 0 = width, 1 = height, 2 = back
-        int width;
-        int height;
+        readonly int minWidth = 4;
+        readonly int minHeight = 3;  
+        int width = 10;
+        int height = 9;
+        int startingLength = 3;
         ConsoleKey input;
-
-        void DrawTitle()
-        {
-            for (int i = 0; i < title.Length; i++)
-            {
-                Console.CursorLeft = Console.WindowWidth / 2 - title[i].Length / 2;
-                Console.ForegroundColor = i % 2 == 0 ? ConsoleColor.Red : ConsoleColor.DarkRed;
-                Console.WriteLine(title[i]);
-            }
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-
-        void DrawItems(string[] items, int selected)
-        {
-            for (int i = 0; i < items.Length; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Console.WriteLine();
-                }
-
-                if (selected == i)
-                {
-                    Console.CursorLeft = Console.WindowWidth / 2 - (items[i].Length + 4) / 2;
-                    Console.WriteLine($"[ {items[i]} ]");
-                }
-                else
-                {
-                    Console.CursorLeft = Console.WindowWidth / 2 - items[i].Length / 2;
-                    Console.WriteLine(items[i]);
-                }
-            }
-        }
-
-        void DrawMainMenu()
-        {
-            Console.Clear();
-            DrawTitle();
-            DrawItems(mainMenuItems, mainMenuSelected);
-
-            Console.ForegroundColor = ConsoleColor.White;
-            input = Console.ReadKey(true).Key;
-
-            if (input == ConsoleKey.DownArrow)
-            {
-                if (mainMenuSelected == mainMenuItems.Length - 1)
-                {
-                    mainMenuSelected = 0;
-                }
-                else
-                {
-                    mainMenuSelected++;
-                }
-            }
-            else if (input == ConsoleKey.UpArrow)
-            {
-                if (mainMenuSelected == 0)
-                {
-                    mainMenuSelected = mainMenuItems.Length - 1;
-                }
-                else
-                {
-                    mainMenuSelected--;
-                }
-            }
-
-            if (input == ConsoleKey.Enter)
-            {
-                switch (mainMenuSelected)
-                {                   
-                    case 0:
-                        gameState = GameState.Start;
-                        break;
-                    case 1:
-                        gameState = GameState.Settings;
-                        break;
-                    case 2:
-                        gameState = GameState.Quit;
-                        break;
-                }
-                mainMenuSelected = 0;
-            }
-        }
-
-        void DrawSettings()
-        {
-            Console.Clear();
-            DrawTitle();
-            DrawItems(settingsItems, settingsSelected);
-
-            input = Console.ReadKey(true).Key;     
-
-            if (input == ConsoleKey.DownArrow)
-            {
-                if (settingsSelected == settingsItems.Length - 1)
-                {
-                    settingsSelected = 0;
-                }
-                else
-                {
-                    settingsSelected++;
-                }
-            }
-            else if (input == ConsoleKey.UpArrow)
-            {
-                if (settingsSelected == 0)
-                {
-                    settingsSelected = settingsItems.Length - 1;
-                }
-                else
-                {
-                    settingsSelected--;
-                }
-            }
-
-            if (input == ConsoleKey.Enter)
-            {
-                switch (settingsSelected)
-                {
-                    case 0:
-                        gameState = GameState.SettingsBoard;
-                        break;
-                    case 1:
-                        gameState = GameState.SettingsSnake;
-                        break; 
-                    case 2:
-                        gameState = GameState.SettingsApples;
-                        break;
-                    case 3:
-                        gameState = GameState.MainMenu;
-                        break;
-                }
-                settingsSelected = 0;
-            }
-        }
-
-        void DrawSettingsBoard()
-        {
-            Console.Clear();
-            DrawTitle();
-            DrawItems(settingsBoardItems, settingsBoardSelected);
-
-
-
-            if (input == ConsoleKey.DownArrow)
-            {
-                if (settingsBoardSelected == settingsBoardItems.Length - 1)
-                {
-                    settingsBoardSelected = 0;
-                }
-                else
-                {
-                    settingsBoardSelected++;
-                }
-            }
-            else if (input == ConsoleKey.UpArrow)
-            {
-                if (settingsBoardSelected == 0)
-                {
-                    settingsBoardSelected = settingsBoardItems.Length - 1;
-                }
-                else
-                {
-                    settingsBoardSelected--;
-                }
-            }
-
-            input = Console.ReadKey().Key;
-
-            if (input == ConsoleKey.Enter)
-            {
-                switch (settingsBoardSelected)
-                {
-                    case 0:
-                        
-                        break;
-                    case 1:
-                        
-                        break;
-                    case 2:
-                        gameState = GameState.Settings;
-                        break;
-                    
-                }
-                settingsSelected = 0;
-            }
-        }
+        Snake snake;
 
         void StartGame()
         {
             board = new int[width, height];
-
-
+            snake.X = startingLength;
+            snake.Y = height / 2;
+            snake.Length = startingLength;
 
             gameState = GameState.Playing;
         }
